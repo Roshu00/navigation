7178da7030ef20b7d3def3b2b4e9c349b6086e2b
Roshu00 <urosninkovic@yahoo.com>
2022-03-08 18:18:08 +0100

    cost function calculating finish

--- src/utils/navigationFunction.js
+++ src/utils/navigationFunction.js
@@ -1,5 +1,16 @@
-const fCostCalculate = (x, y, endCell, startCell) => {
-  const gCost = "";
+const costCalculate = (cell, endCell, startCell) => {
+  console.log("CALCUlATING");
+  const XgCost = Math.abs(cell.x - startCell.x);
+  const YgCost = Math.abs(cell.y - startCell.y);
+  const gCost = Math.abs(YgCost - XgCost) * 10 + Math.min(YgCost, XgCost) * 14;
+
+  const XhCost = Math.abs(cell.x - endCell.x);
+  const YhCost = Math.abs(cell.y - endCell.y);
+  const hCost = Math.abs(YhCost - XhCost) * 10 + Math.min(YhCost, XhCost) * 14;
+
+  const fCost = gCost + hCost;
+
+  return { gCost, hCost, fCost };
 };
 
 export const navigationF = (
@@ -14,56 +25,64 @@ export const navigationF = (
     const y = startCell.y;
     const x = startCell.x - 1;
     if (!wallCells.some((cell) => cell.x === x && cell.y === y)) {
-      selectedCells.push({ x, y });
+      const cost = costCalculate({ x, y }, endCell, endCell);
+      selectedCells.push({ x, y, cost });
     }
   }
   if (startCell.x + 1 < borderX) {
     const y = startCell.y;
     const x = startCell.x + 1;
     if (!wallCells.some((cell) => cell.x === x && cell.y === y)) {
-      selectedCells.push({ x, y });
+      const cost = costCalculate({ x, y }, endCell, endCell);
+      selectedCells.push({ x, y, cost });
     }
   }
   if (startCell.y - 1 > 0) {
     const y = startCell.y - 1;
     const x = startCell.x;
     if (!wallCells.some((cell) => cell.x === x && cell.y === y)) {
-      selectedCells.push({ x, y });
+      const cost = costCalculate({ x, y }, endCell, endCell);
+      selectedCells.push({ x, y, cost });
     }
   }
   if (startCell.y + 1 < borderY) {
     const y = startCell.y + 1;
     const x = startCell.x;
     if (!wallCells.some((cell) => cell.x === x && cell.y === y)) {
-      selectedCells.push({ x, y });
+      const cost = costCalculate({ x, y }, endCell, endCell);
+      selectedCells.push({ x, y, cost });
     }
   }
   if (startCell.y - 1 > 0 && startCell.x - 1 > 0) {
     const y = startCell.y - 1;
     const x = startCell.x - 1;
     if (!wallCells.some((cell) => cell.x === x && cell.y === y)) {
-      selectedCells.push({ x, y });
+      const cost = costCalculate({ x, y }, endCell, endCell);
+      selectedCells.push({ x, y, cost });
     }
   }
   if (startCell.y - 1 > 0 && startCell.x + 1 < borderX) {
     const y = startCell.y - 1;
     const x = startCell.x + 1;
     if (!wallCells.some((cell) => cell.x === x && cell.y === y)) {
-      selectedCells.push({ x, y });
+      const cost = costCalculate({ x, y }, endCell, endCell);
+      selectedCells.push({ x, y, cost });
     }
   }
   if (startCell.y + 1 < borderY && startCell.x - 1 > 0) {
     const y = startCell.y + 1;
     const x = startCell.x - 1;
     if (!wallCells.some((cell) => cell.x === x && cell.y === y)) {
-      selectedCells.push({ x, y });
+      const cost = costCalculate({ x, y }, endCell, endCell);
+      selectedCells.push({ x, y, cost });
     }
   }
   if (startCell.y + 1 < borderY && startCell.x + 1 < borderX) {
     const y = startCell.y + 1;
     const x = startCell.x + 1;
     if (!wallCells.some((cell) => cell.x === x && cell.y === y)) {
-      selectedCells.push({ x, y });
+      const cost = costCalculate({ x, y }, endCell, endCell);
+      selectedCells.push({ x, y, cost });
     }
   }
   return selectedCells;
